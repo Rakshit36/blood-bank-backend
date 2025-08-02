3db2827dfe4b69662b840b1cc35b31a09d26fd36
Gursewakzopdev <gursewak.singh@zop.dev>
2025-08-02 16:45:40 +0530

    temp

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(
     cors({
         origin: [
-            "https://blood-bank-frontend-silk.vercel.app/",
+            "*"
         ],
         credentials: true,
     })
